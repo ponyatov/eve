769e9e2bab0490411978a96d1f6dae7ee601ec35
Dmitry Ponyatov <dponyatov@gmail.com>
2019-08-05 00:34:12 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -5,6 +5,13 @@
 void yyerror(string msg) {cout<<YYERR; cerr<<YYERR; exit(-1);}
 
 int main(int argc, char *argv[]) {
-    cout << argv[0] << endl;
+    cout << "## " << argv[0] << endl;
     return yyparse();
 }
+
+Frame::Frame(string T, string V)    { type = T; val = V; }
+
+Num::Num(string V):Frame("num",V)   { val = atof(V); }
+Int::Int(string V):Num("int",V)     { val = strtol(V,NULL,0x0A); }
+Hex::Hex(string V):Num("hex",V)     { val = strtol(V,NULL,0x10); }
+Bin::Bin(string V):Num("bin",V)     { val = strtol(V,NULL,0x02); }
--- hpp.hpp
+++ hpp.hpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
+struct Frame {
+    string type;
+    string val;
+    Frame(string T, string V);
+    virtual string head(string prefix="");
+};
+
+struct Str:Frame { Str(string); };
+
+struct Num:Frame { float val; Num(string); };
+struct Int:Num   { long  val; Int(string); };
+struct Hex:Int   {            Hex(string); };
+struct Bin:Int   {            Bin(string); };
+
 extern int yylex();
 extern int yylineno;
 extern char *yytext;
 extern int yyparse();
 extern void yyerror(string msg);
 #include "ypp.tab.hpp"
+
